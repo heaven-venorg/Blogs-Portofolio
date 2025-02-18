144a438a0aa65daff8e4da054b4ac1e99d38d0e0
heaven.venorg <heavensipangkar1@gmail.com>
2025-02-18 18:15:27 +0700

    update validasi

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,7 +5,13 @@
         <form action="{{ route('post.store') }}" method="post" class="w-75 d-flex flex-column gap-2">
             @csrf
             <input type="text" name="title" class="form-control" placeholder="Masukan judul artikel">
+            @if ($errors->has('title'))
+                <div class="alert alert-warning">Title Wajib Diisi</div>
+            @endif
             <textarea name="content" rows="15" class="form-control" placeholder="Masukan content artikel"></textarea>
+            @if ($errors->has('content'))
+                <div class="alert alert-warning">Content Wajib Diisi</div>
+            @endif
             <div class="group d-flex">
                 <input type="reset" value="Kosongkan" class="btn btn-dark w-25 d-flex me-auto">
                 <input type="submit" value="Tambah Postingan" class="btn btn-primary w-25 d-flex ms-auto">
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -2,19 +2,20 @@
 @section('content')
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center w-100">
         <h1>Form Edit Postingan</h1>
-        <form action="{{ route('post.update', $post->id) }}" method="post" class="w-75 d-flex flex-column gap-2">
+        <form action="{{ route('post.edit', $post->id) }}" method="post" class="w-75 d-flex flex-column gap-2">
             @csrf
             @method('PUT')
-            <input type="text" name="title" class="form-control" placeholder="Masukan judul artikel"
-                value="{{ $post->title }}">
-            <textarea name="content" rows="10" class="form-control" placeholder="Masukan content artikel">{{ $post->content }}</textarea>
-            <select class="form-select" name="visibility">
-                <option value="public">Public</option>
-                <option value="private">Private</option>
-            </select>
+            <input type="text" name="title" class="form-control" placeholder="Masukan judul artikel">
+            @if ($errors->has('title'))
+                <div class="alert alert-warning">Title Wajib Diisi</div>
+            @endif
+            <textarea name="content" rows="15" class="form-control" placeholder="Masukan content artikel"></textarea>
+            @if ($errors->has('content'))
+                <div class="alert alert-warning">Content Wajib Diisi</div>
+            @endif
             <div class="group d-flex">
                 <input type="reset" value="Kosongkan" class="btn btn-dark w-25 d-flex me-auto">
-                <input type="submit" value="Update Postingan" class="btn btn-primary w-25 d-flex ms-auto">
+                <input type="submit" value="Tambah Postingan" class="btn btn-primary w-25 d-flex ms-auto">
             </div>
         </form>
     </div>
